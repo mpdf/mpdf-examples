473ca969dab6d5596f8512e27cbc3e3f2d522a1e
Matěj Humpál <finwe@finwe.info>
2019-03-07 10:15:39 +0100

    New parametrized example generating shell script

---
+++ generate-examples.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+trap "echo Exited!; exit;" SIGINT SIGTERM
+
+if [ $# -eq 0 ]; then
+    echo "usage ./generate-examples.sh <PHP version>[ <path to mPDF>]"
+    exit 1
+fi
+
+export MPDF_ROOT=${2:-"$PWD"}
+
+if [ ! -f "${MPDF_ROOT}/vendor/autoload.php" ]; then
+    echo "Unable to find composer autoload in given mPDF path ${MPDF_ROOT}"
+    exit 1
+fi
+
+files=(./example??_*.php)
+echo "Total files in list: ${#files[*]}, mPDF path ${MPDF_ROOT}"
+for var in "${files[@]}"
+do
+  var="$(sed s/^\\\.\\\///g <<<$var)"
+  mkdir -p "examples${1}"
+  /usr/bin/time -f "%e ${var}" php${1} "${var}" > "examples${1}/${var}.pdf"
+done
+exit
