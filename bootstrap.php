db67262342e9a14a116714a7a7b209acc0a8f4ce
Matěj Humpál <finwe@finwe.info>
2019-11-21 20:08:55 +0100

    Fix vendor path in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
 // require composer autoload
-$path = (getenv('MPDF_ROOT')) ? getenv('MPDF_ROOT') : __DIR__;
+$envRoot = getenv('MPDF_ROOT');
+$path = $envRoot ?: __DIR__;
 
-require_once $path . '/../repo/vendor/autoload.php';
+require_once $path . '/vendor/autoload.php';
 
 Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT, __DIR__ . '/log');
 Tracy\Debugger::$strictMode = true;
