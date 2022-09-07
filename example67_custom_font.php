8aa33242a8cb08a900132fefb4ba28f28be5695e
Matěj Humpál <finwe@finwe.info>
2022-09-07 09:39:59 +0200

    Wider custom fonts example

--- example67_custom_font.php
+++ example67_custom_font.php
@@ -11,19 +11,37 @@ $fontData = $defaultFontConfig['fontdata'];
 
 $mpdf = new \Mpdf\Mpdf([
 	'fontDir' => array_merge($fontDirs, [__DIR__]),
-	'fontdata' => $fontData + ['angerthas' => [
-		'R' => 'assets/angerthas.ttf',
-	]],
-	'default_font' => 'angerthas'
+	'fontdata' => $fontData + 
+		[
+			'angerthas' => [
+				'R' => 'assets/angerthas.ttf',
+			],
+			'inkfree' => [
+				'R' => 'assets/Inkfree.ttf',
+			],
+		],
 ]);
 
 $mpdf->WriteHtml('<html>
     <head>
+		<style>
+		.inkfree {
+			font-family: "Ink Free";
+		}
+		</style>
     </head>
     <body>
 <h1>Using custom font in the document</h1>
 
-<p>This example shows how to keep default font families while adding a custom font directory and definitions.
+<p style=\'font-family: angerthas\'>This example shows how to keep default font families while adding a custom font directory and definitions.</p>
+
+<p style="font-family: \'Ink Free\'">Inkfree line of text</p>
+
+<p style="font-family: "Ink Free";">Inkfree line of text that is not working</p>
+
+<p style=\'font-family: "Ink Free"\'>Inkfree line of text that is not working</p>
+
+<p class="inkfree">Inkfree line of text</p>
 
 </body>
 </html>');
